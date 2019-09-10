f22cf2092448f7a3bfb64e9355df6308195d3d5e
SabrinaKudo <49425725+SabrinaKudo@users.noreply.github.com>
2019-09-09 18:18:54 -0700

    files
    uploaded

---
+++ jquery/index.js
@@ -0,0 +1,3 @@
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+<script src="http://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.js"></script>
+
